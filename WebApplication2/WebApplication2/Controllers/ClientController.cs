ee13ea3f6d82d222a5b7ff3f0a6c559c2917e279
Marcel <129780388+Marceli-Chlebowski@users.noreply.github.com>
2024-06-08 01:26:09 +0200

    ...

---
+++ WebApplication2/WebApplication2/Controllers/ClientController.cs
@@ -0,0 +1,44 @@
+﻿using APBD9.Data;
+using APBD9.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace APBD9.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ClientController : ControllerBase
+{
+    private readonly ApbdContext _context;
+
+    public ClientController(ApbdContext context)
+    {
+        _context = context;
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteClient(int id)
+    {
+        var clientsTrips = await _context.ClientTrips.Where(c => c.IdClient == id).ToListAsync();
+        
+        if (clientsTrips.Any())
+        {
+            return StatusCode(403, "Unacceptable: The client has assigned trips and cannot be deleted.");
+        }
+        else
+        {
+            var client = await _context.Clients.FirstOrDefaultAsync(c => c.IdClient == id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+            
+            _context.Clients.Remove(client);
+            await _context.SaveChangesAsync();
+            return Ok("Client deletet succesfully");
+        }
+    }
+}
---
+++ WebApplication2/WebApplication2/Controllers/TripsControllers.cs
@@ -0,0 +1,77 @@
+﻿using APBD9.Data;
+using APBD9.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APBD9.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TripsControllers: ControllerBase
+{
+    private readonly ApbdContext _context;
+
+    public TripsControllers(ApbdContext _context)
+    {
+        this._context = _context;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetTrips()
+    {
+        var trips = await _context.Trips.ToListAsync();
+        return Ok(trips);
+    }
+
+    [HttpGet("trips")]
+    public async Task<IActionResult> GetTripsSortedAndPaged(int? page, int pageSize = 10)
+    {
+        int pageNumber = page ?? 1;
+        int skipAmount = (pageNumber - 1) * pageSize;
+
+        var trips = await _context.Trips
+            .OrderByDescending(t => t.DateFrom)
+            .Skip(skipAmount)
+            .Take(pageSize)
+            .ToListAsync();
+
+        return Ok(trips);
+    }
+
+    [HttpPost("{idTrip}/clients")]
+    public async Task<IActionResult> AssignClientToTrip(int idTrip, ClientTripDTO clientTripDTO)
+    {
+        var trip = await _context.Trips.FirstOrDefaultAsync(t => t.IdTrip == idTrip && t.DateFrom > DateTime.Now);
+        if (trip == null)
+        {
+            return NotFound("Trip does not exist or has already started.");
+        }
+
+        var client = await _context.Clients.FirstOrDefaultAsync(c => c.Pesel == clientTripDTO.Pesel);
+        if (client == null)
+        {
+            return NotFound("Client does not exist.");
+        }
+
+        bool isAlreadyAssigned = await _context.ClientTrips.AnyAsync(ct => ct.IdTrip == idTrip && ct.IdClient == client.IdClient);
+        if (isAlreadyAssigned)
+        {
+            return BadRequest("Client is already assigned to this trip.");
+        }
+
+        var clientTrip = new ClientTrip()
+        {
+            IdTrip = idTrip,
+            IdClient = client.IdClient,
+            RegisteredAt = DateTime.UtcNow,
+            PaymentDate = clientTripDTO.PaymentDate
+        };
+
+        _context.ClientTrips.Add(clientTrip);
+        await _context.SaveChangesAsync();
+
+        return Ok("Client assigned to trip successfully.");
+    }
+
+
+}
